0a44941ad6469f71cf7a88481d89ab6d2432e9fe
Mahalakshmisubramoniam <mahasubramoniam19@gmail.com>
2022-09-03 11:30:11 +0530

    first commit

---
+++ app.py
@@ -0,0 +1,18 @@
+name = input("what is your name: ")
+age = input("how old are you: ")
+membership = input("do you have membership: ")
+print(name)
+print(type(age))
+print(membership)
+if membership=="yes":
+    print("please log in")
+else:
+    print("get your membership")
+
+
+
+
+
+
+
+
---
+++ tutorial.py
@@ -0,0 +1,8 @@
+# NAME=1
+# print(id(name))
+# name=0
+# print(id(name))
+# name="maha"
+# p
+int6=0
+print(int6)
